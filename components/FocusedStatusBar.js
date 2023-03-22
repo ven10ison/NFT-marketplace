57776f4202f5cb18f6ea358cbc84df6bbe24db9f
Alven Asdal <supportingenterpriseinc.@Supportings-MacBook-Pro.local>
2023-03-22 18:30:48 +0800

    First commit

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { StatusBar} from 'react-native'
+import { useIsFocused } from '@react-navigation/core'
+
+
+const FocusedStatusBar = (props) => {
+
+  const isFocued = useIsFocused();
+
+  return isFocued ? <StatusBar animated={true} {...props}/> : null;
+}
+
+export default FocusedStatusBar
---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar'
+import NFTCard from './NFTCard'
+import { CircleButton, RectangleButton } from './Buttons'
+import DetailsDesc from './DetailsDesc'
+import DetailsBid from './DetailsBid'
+import { SubInfo } from './SubInfo';
+
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCard,
+    CircleButton,
+    RectangleButton,
+    DetailsDesc,
+    DetailsBid,
+    SubInfo
+}
